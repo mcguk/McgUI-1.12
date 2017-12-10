64731e1d2fbe7ad92e142620113ae0afc5ef4610
McgGithub <dmcguk@msn.com>
2017-12-10 22:03:29 +0000

    update

--- WTF/Account/YourAccountName/SavedVariables/npcscan.lua
+++ WTF/Account/YourAccountName/SavedVariables/npcscan.lua
@@ -1,12 +1,4 @@
 
 npcscan_targets = {
-	["BLOODFALCON"] = true,
-	["REDBRINGER"] = true,
-	["ANTENNAE"] = true,
-	["TWOTWO"] = true,
-	["THANGORE"] = true,
-	["GISKA"] = true,
-	["MUJAKI"] = true,
-	["BDHUMTSS"] = true,
-	["CILVAQT"] = true,
+
 }
