350766c7dabb467dc191a51c76d67f62e97b287d
McgGithub <dmcguk@msn.com>
2017-12-11 22:21:04 +0000

    update

--- WTF/Account/YourAccountName/Kronos/Warrior/SavedVariables/KLHThreatMeter.lua
+++ WTF/Account/YourAccountName/Kronos/Warrior/SavedVariables/KLHThreatMeter.lua
@@ -20,7 +20,7 @@ KLHTM_SavedVariables = {
 					["threat"] = true,
 					["name"] = true,
 					["pc"] = true,
-					["rage"] = true,
+					["rage"] = false,
 					["hits"] = true,
 				},
 				["sortColumn"] = "threat",
